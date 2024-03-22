95bfe308855b52c7a67b5a4f7de99948caffcf76
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-03-21 21:36:20 -0300

    Adicionar arquivos de projeto.

---
+++ CP1-MVC/Controllers/FirstController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CP1_MVC.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult First()
+        {
+            return View();
+        }
+    }
+}
---
+++ CP1-MVC/Controllers/SecondController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CP1_MVC.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Second()
+        {
+            return View();
+        }
+    }
+}
